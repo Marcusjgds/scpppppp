a67d7c30c4eb901b3bfbaded123d6953a2a7fa05
Marcusjgds <marcus.tramecon@gmail.com>
2026-05-29 22:30:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,6 +8,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log('Listening on port ' + PORT);
 });
